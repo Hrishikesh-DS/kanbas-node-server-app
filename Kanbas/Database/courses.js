3338b1ea845e9df5a5aa28ed077a57b897c9b19f
hrishikeshds <dshrishikesh@gmail.com>
2024-04-02 20:31:52 -0400

    first commit

---
+++ Kanbas/Database/courses.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    "_id": "RS101",
+    "name": "Web Development",
+    "number": "RS4550",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "../logo192.png"
+  },
+  {
+    "_id": "RS102",
+    "name": "Database Management System",
+    "number": "RS4560",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "../logo192.png"
+  },
+  {
+    "_id": "RS103",
+    "name": "Algorithms",
+    "number": "RS4570",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "../logo192.png"
+  },
+  {
+    "_id": "RS104",
+    "name": "PDP",
+    "number": "RS4580",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "../logo192.png"
+  }
+]
