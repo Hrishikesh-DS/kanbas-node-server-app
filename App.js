0111d8556c5d6d3bec76aff480842fce77d85054
hrishikeshds <dshrishikesh@gmail.com>
2024-04-11 18:29:09 -0400

    bug fix

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: process.env.FRONTEND_URL
+        origin: process.env.FRONTEND_URL||'/https://kanbas-node-server-app-a6-iwky.onrender.com'
     })
 );
 const sessionOptions = {
