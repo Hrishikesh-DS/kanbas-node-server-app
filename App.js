1b98c5c60f0a6faa62de92e49b09a3eed38048f6
hrishikeshds <dshrishikesh@gmail.com>
2024-04-11 21:31:06 -0400

    proxy

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-app.set('trust proxy', 1);
+// app.set('trust proxy', 1);
 app.use(
     cors({
         credentials: true,
@@ -24,6 +24,7 @@ const sessionOptions = {
     secret: process.env.SESSION_SECRET || 'AWZPH5450Q',
     resave: false,
     saveUninitialized: false,
+    proxy: true,
     cookie: {
         sameSite: 'none',
         secure: true,
